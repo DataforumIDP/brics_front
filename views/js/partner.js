d961f6fbc0c3acf36b110e0c09cf1e06fcaacfc2
Daniel <daniel.vn.rus@gmail.com>
2024-06-26 15:49:54 +0300

    Update

--- views/js/partner.js
+++ views/js/partner.js
@@ -1,17 +1,21 @@
 import { toggleEditMode } from "./modules/editPartner.js";
 import { exit } from "./modules/exit.js";
 import { fillPartnerData, getPartnerData } from "./modules/getPartnersData.js";
+import { closePartnerScreen, openPartnerScreen } from "./modules/partnerScreen.js";
 import { scrollbarInit } from "./modules/scrollBarInit.js";
 
 scrollbarInit()
 
 $('.exit__content').click(exit)
 
-st()
+start()
 
-async function st() {
+async function start() {
     const [{partner}] = await getPartnerData()
     fillPartnerData(partner)
 }
 
 $('.edit__btn').click(toggleEditMode)
+
+$(document).on('click', '.list__btn:not(.--grey)', openPartnerScreen)
+$('.back-to-profile').click(closePartnerScreen)

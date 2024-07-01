118f866cb34f4ca337563a781f536a74132a27a8
Daniel <daniel.vn.rus@gmail.com>
2024-07-01 13:54:41 +0300

    Fix

--- views/js/modules/editPartner.js
+++ views/js/modules/editPartner.js
@@ -91,7 +91,7 @@ export function openEditUser (id) {
 
     $('.--id span').text(user.id)
     $('.--date span').text(`${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`)
-    $('.--e-fio').val(`${user.surname} ${user.name} ${user.lastname??''}`)
+    $('.--e-fio').val(`${user.surname} ${user.name} ${user.lastname==null ? '' : user.lastname}`)
     $('.--e-passport').val(user.passport)
     $('.--e-grade').val(user.grade)
     $('.--e-activity').val(user.activity)
